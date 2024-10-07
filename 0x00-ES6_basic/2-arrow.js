30c6730c45f7dbe2359208b44db95953cd8464d4
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-07 11:10:24 +0300

    done

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    const addNeighborhood = (newNeighborhood) => {
-        self.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return self.sanFranciscoNeighborhoods;
-    };
-    return {addNeighborhood}
-  }
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  const addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+  return { addNeighborhood };
+}
