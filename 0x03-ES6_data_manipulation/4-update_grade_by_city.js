0d9408942f1ea8e1b9d5904f15c7c9844861b92f
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-08 21:15:28 +0300

    done

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity (arr, str, grades) {
+    const students = arr.filter(a => a.location == str)
+    students.forEach(element => {
+        grades.forEach(g => {
+            if (element.id == g.studentId)
+            {
+                element.grade = g.grade
+            }
+        })
+        if (!('grade' in element))
+        {
+            element.grade = 'N/A'
+        }
+    });
+    return students
+}
