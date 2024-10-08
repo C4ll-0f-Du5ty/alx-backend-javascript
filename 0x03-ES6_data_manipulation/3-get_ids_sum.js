809e4ac149939c68c336d2303727e926dc6f8b41
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-08 20:04:01 +0300

    done

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentIdsSum (arr) {
-    return arr.reduce((accum, current) => accum + current.id, 0)
+export default function getStudentIdsSum(arr) {
+  return arr.reduce((accum, current) => accum + current.id, 0);
 }
