28dd51ecf0e7f4f0b7157da43e9f71356f60d654
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-08 21:18:18 +0300

    done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,14 @@
-export default function updateStudentGradeByCity (arr, str, grades) {
-    const students = arr.filter(a => a.location == str)
-    students.forEach(element => {
-        grades.forEach(g => {
-            if (element.id == g.studentId)
-            {
-                element.grade = g.grade
-            }
-        })
-        if (!('grade' in element))
-        {
-            element.grade = 'N/A'
-        }
+export default function updateStudentGradeByCity(arr, str, grades) {
+  const students = arr.filter((a) => a.location === str);
+  students.forEach((element) => {
+    grades.forEach((g) => {
+      if (element.id === g.studentId) {
+        element.grade = g.grade;
+      }
     });
-    return students
+    if (!('grade' in element)) {
+      element.grade = 'N/A';
+    }
+  });
+  return students;
 }
