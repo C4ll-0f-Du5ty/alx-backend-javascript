550cf2d5db5f692dfbc2fa3bcbbf7d88a2154593
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-08 19:53:25 +0300

    done

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,4 @@
-
-export default function getListStudentIds (arr) {
-
-    return (Array.isArray(arr) ? arr.map(obj => obj.id) : [])
-    // console.log(ids)
+export default function getListStudentIds(arr) {
+  return (Array.isArray(arr) ? arr.map((obj) => obj.id) : []);
+  // console.log(ids)
 }
