8b47f31477ce8b5c3ffb8a9f148d0c1ec7811483
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-08 19:57:54 +0300

    done

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation (arr, str) {
-    return arr.filter(obj => obj.location == str)
+export default function getStudentsByLocation(arr, str) {
+  return arr.filter((obj) => obj.location == str);
 }
